155ab1786fdfa08eb46138796cab10a029d45d97
Takudzwa Mupanesure <zhout1709@hotmail.com>
2021-10-05 20:08:36 +0200

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 @Component({
-  selector: 'ss-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'ss-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = '#RegisterToVoteZW';
-  isMenuCollapsed = true;
+    title = 'Video Player Showcase';
+    isMenuCollapsed = true;
 
-  toggleMenu(): void {
-    this.isMenuCollapsed = !this.isMenuCollapsed;
-  }
+    toggleMenu(): void {
+        this.isMenuCollapsed = !this.isMenuCollapsed;
+    }
 }
